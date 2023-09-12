854e2c8fab198b7ffc346ee796b4c3c4e2f8565d
OTÁVIO ROSA ZAMPOLLI <otavio.rz@puccampinas.edu.br>
2023-09-12 16:04:21 -0300

    troca de .html para .php e apaguei o index.php antigo

--- php/src/index.php
+++ php/src/index.php
@@ -1,20 +1,63 @@
-<?php
-//These are the defined authentication environment in the db service
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
 
-// The MySQL service named in the docker-compose.yml.
-$host = 'db';
+<body>
+        <table>
+        <caption>
+            Produtos cadastrados
+        </caption>
+        <thead>
+            <tr>
+                <th>Preço</th>
+                <th>Nome</th>
+                <th>Quantidade</th>
 
-// Database use name
-$user = 'MYSQL_USER';
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>2</td>
+                <td>3</td>
 
-//database user password
-$pass = 'MYSQL_PASSWORD';
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>5</td>
+                <td>6</td>
 
-// check the MySQL connection status
-$conn = new mysqli($host, $user, $pass);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connected to MySQL server successfully!";
-}
-?>
+            </tr>
+            <tr>
+                <td>7</td>
+                <td>8</td>
+                <td>9</td>
+                <tr>
+                    <td>10</td>
+                    <td>11</td>
+                    <td>12</td>
+    
+                </tr>
+                <tr>
+                    <td>13</td>
+                    <td>14</td>
+                    <td>15</td>
+    
+                </tr>
+                <tr>
+                    <td>16</td>
+                    <td>17</td>
+                    <td>18</td>
+
+            </tr>
+        </tbody>
+    </table>
+    
+</body>
+</html>
